62bacd2902676501abcf70cf7148b7e236bf4d71
Nurul Islam <nurulbdpersonal@gmail.com>
2022-08-11 19:57:03 +0600

    case.js update

--- case.js
+++ case.js
@@ -1,8 +1,56 @@
+// const userName = 'blackPink';
+// const userInput = 'blackPink';
+
+// if( userName === userInput){
+//     console.log('valid user')
+// }
+// else {
+//     console.log('invalid user');
+// } // print: valid user
+
+
+
+// const userName = 'blackPink';
+// const userInput = 'blackPinK';
+
+// if( userName === userInput){
+//     console.log('valid user')
+// }
+// else {
+//     console.log('invalid user');
+// } // print: invalid user
+
+
+
+// const userName = 'blackPink';
+// const userInput = 'blackPinK';
+
+// if( userName.toLowerCase() === userInput.toLowerCase()){
+//     console.log('valid user')
+// }
+// else {
+//     console.log('invalid user');
+// } // print: valid user
+
+
+
+// const userName = 'blackPink';
+// const userInput = 'blackPinK';
+
+// if( userName.toUpperCase() === userInput.toUpperCase()){
+//     console.log('valid user')
+// }
+// else {
+//     console.log('invalid user');
+// } // print: valid user
+
+
+
 const userName = 'blackPink';
 const userInput = 'blackPinK';
-console.log(userName.toLowerCase());
-console.log(userInput.toUpperCase());
-console.log(userName.toLocaleLowerCase())
+// console.log(userName.toLowerCase());
+// console.log(userInput.toUpperCase());
+// console.log(userName.toLocaleLowerCase())
 if( userName.toLowerCase() === userInput.toLowerCase()){
     console.log('valid user')
 }
